64efb0b6d7d38b326e00967ffe78308976b1f394
DarkXPixel <nahmurin.dima65@gmail.com>
2024-12-23 20:04:19 +0300

    Update Geometry.h

--- Dark Engine/Source/Runtime/DarkUI/Public/Layout/Geometry.h
+++ Dark Engine/Source/Runtime/DarkUI/Public/Layout/Geometry.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Platform/Platform.h"
+#include "Math/MathFwd.h"
+#include "Math/Vector2.h"
 
 struct FGeometry
 {
@@ -8,5 +10,11 @@ public:
 
 	FGeometry(const FGeometry& RHS) = default;
 
+	FGeometry(const FVector2f& OffsetFromParent, 
+		const FVector2f& ParentAbsolutePosition, 
+		const FVector2f& InLocalSize,
+		float InScale);
 
+
+private:
 };
